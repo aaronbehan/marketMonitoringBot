35eaa234829101d81d40328ce5146399ab3a1e99
Aaron <117947303+aaronbehan@users.noreply.github.com>
2025-01-30 15:15:09 +0000

    Update bot.py
    The bot captures a small screenshot of the price. 
    I have incorporated text from image recognition (OCR) using Tesseract.

--- bot.py
+++ bot.py
@@ -4,10 +4,13 @@ import time
 import keyboard
 import random
 import win32api, win32con
+import pytesseract
+from PIL import Image
+
 
 # DOES NOT RUN ON VSCODE OR PYCHARM. ONLY RUNS ON IDLE
 
-items_of_interest = ["goose friend", "gnoll", "forever prom queen", "crate of dynamite"]
+items_of_interest = ["gnoll"]
 
 while True:
     if keyboard.is_pressed('p'):
@@ -17,16 +20,43 @@ while True:
 
         if search_bar != None and search_button != None:
             for item in items_of_interest:
-
+                
+                # Entering search parametres and clicking search
                 pyautogui.click((search_bar.left + search_bar.width * 2), (search_bar.top + search_bar.height / 2))  # moves mouse to X, Y coords then clicks
                 time.sleep(0.3)
                 pyautogui.doubleClick((search_bar.left + search_bar.width * 2), (search_bar.top + search_bar.height / 2))
                 pyautogui.write(item)
-                pyautogui.click((search_button.left + search_button.width / 2), (search_button.top + search_button.height / 2))  # moves mouse to X, Y coords then clicks
+                pyautogui.click((search_button.left + search_button.width / 2), (search_button.top + search_button.height / 2))
+                print("Sleeping for 4 seconds")
+                time.sleep(4)
                 
-                time.sleep(2)
-                if pyautogui.locateOnScreen(f'resources/{item}.png'):
+                # Checking top search results against PNG file
+                item_location = pyautogui.locateOnScreen(f'resources/{item}.png')
+
+                if item_location != None:
                     print(f"i see the {item}")
+                    # Pyautogui takes screenshot of price
+                    screenshot = pyautogui.screenshot("screenshot.png", region=(item_location.left + item_location.width, item_location.top, 80, 40))
+                    print("Screenshot taken")
+
+                    # Converting screenshot into string using pytesseract
+                    img_file = "screenshot.png"
+                    img = Image.open(img_file)
+                    ocr_result = pytesseract.image_to_string(img)
+                    print(f"ocr_result = {ocr_result}")
+
+                    # Converting string into list and looking for word: "price"
+                    result_as_list = ocr_result.split()
+                    print(result_as_list)
+                    for word in result_as_list:
+                        if word.lower() == "price:":
+                            index = result_as_list.index(word)
+                            price = result_as_list[index + 1]
+                            print(f"the pricerino is {price}")
+                            break  # only interested in the first instance of price
+                    
                 else:
                     print(f"i don't see the {item}")
+
+
     
